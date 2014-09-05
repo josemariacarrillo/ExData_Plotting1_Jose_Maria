4a9f4dbc16d86d64450d59abbc0c85ea4532da8f
josemariacarrillo <elretornodelesloveno@yahoo.es>
2014-09-05 15:18:13 -0300

    Update Plot3.r

--- Plot3.r
+++ Plot3.r
@@ -1,15 +1,9 @@
 
-plot3 <- function (){
+plot3 <- function (input){
         
-        if (!file.exists("coursera_output")){
-                dir.create("coursera_output")
-        }
+        library(colbycol)
         
-        download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip","./coursera_output/exdata-data-household_power_consumption.zip.")
-        
-        mydata_file_name<-unzip("exdata-data-household_power_consumption.zip")
-        
-        i.can <- cbc.read.table(mydata_file_name, header = TRUE, sep = ";")
+        i.can <- cbc.read.table(input, header = TRUE, sep = ";")
         
         mydata <- as.data.frame(i.can)
         
@@ -30,7 +24,7 @@ plot3 <- function (){
         
         plot3<-with(dat,plot(dat$Datetime,dat$Sub_metering_1, main="", ylab="Energy sub metering",xlab="",cex.lab=0.8, pch=".")) + lines(dat$Datetime,dat$Sub_metering_1,lty=1,cex.lab=0.8,col="purple") + lines(dat$Datetime,dat$Sub_metering_2,lty=1,cex.lab=0.8,col="orange") + lines(dat$Datetime,dat$Sub_metering_3,lty=1,cex.lab=0.8,col="blue")
         plot3<-legend("topright",col=c("purple","orange","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),lwd=1,pt.cex = 1,cex=0.5)
-        dev.copy(png,"./coursera_output/plot3.png")
+        dev.copy(png,"./plot3.png")
         dev.off()
         
         return (plot3)  
